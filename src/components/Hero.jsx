5d2c4c2cacaa59b63f6c3588fbe333694a993bde
Avik Mukherjee <77090462+Avik-creator@users.noreply.github.com>
2023-07-18 22:23:47 +0530

    Added the Required Files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='w-full h-screen'>
+      <img
+        className='top-0 left-0 w-full h-screen object-cover'
+        src='https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg'
+        alt='/'
+      />
+      <div className='bg-black/30 absolute top-0 left-0 w-full h-screen' />
+      <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
+        <div className='md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
+          <p>All Inclusive</p>
+          <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>
+            Private Beaches & Getaways
+          </h1>
+          <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>
+            Helps you find the perfect all-inclusive resort with a private beach. Whether you're looking for a romantic getaway, a family vacation, or a group trip, you'll find the perfect resort for your needs on this website.
+          </p>
+          <button className='bg-white text-black '>Reserve Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
